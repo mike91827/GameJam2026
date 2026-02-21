848be224fa2913c0bbc9858fbb3eb386dce8ead0
mike91827 <mike91827.2015@gmail.com>
2026-02-20 20:17:14 -0600

    added bad fireplace and death

--- GoblinCastle/Global.gd
+++ GoblinCastle/Global.gd
@@ -1,6 +1,9 @@
 extends Node
-
+var timer: float = 0.0
+var interval: float = 1.0
 var loop = 1
+var goingToDie=false
+var GTDCounter = -1;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +11,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if goingToDie:
+		timer+=delta
+		if timer >= interval:
+			timer -= interval
+			GTDCounter-=1
+			
+	if goingToDie && GTDCounter<=0:
+		get_tree().change_scene_to_file("res://DeathScene/DeathScene.tscn")
