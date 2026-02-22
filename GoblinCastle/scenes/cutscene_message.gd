ffd8fb775b69a17c5f0752e7d5b58068cd5e30b3
Lander Joshua Vitug <landv@space-rover.land>
2026-02-22 16:13:35 -0600

    feat: add win sound to cutscene

--- GoblinCastle/scenes/cutscene_message.gd
+++ GoblinCastle/scenes/cutscene_message.gd
@@ -2,12 +2,16 @@ extends Label3D
 
 @export_multiline var cutscene_message: PackedStringArray = ["Thank you!!!", "You summoned me!!", "THANKS FOR PLAYING", "NOW\nYOU'VE\nSUMMONED", "GOBLIN MODE"]
 @export var timer_interval = 0.3
+@export var message_gap = 3
 
 @onready var ThankYouMessage = $"../ThankYou"
+@onready var PlayAgainButton = $"../PlayAgain"
 
 var timer = 0
+var message_timer = 0
 var cur_message = 0
 var cur_letter = 0
+var message_done = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,4 +34,15 @@ func _process(delta: float) -> void:
 				self.text = ""
 				cur_message += 1
 			else:
-				ThankYouMessage.visible = true
+				if not message_done:
+					self.text = "GOBLIN MODE"
+				message_done = true
+	
+	if message_done:
+		message_timer += delta
+		if message_timer >= message_gap and not ThankYouMessage.visible:
+			message_timer = 0
+			self.text = ""
+			ThankYouMessage.visible = true
+		if ThankYouMessage.visible and message_timer >= message_gap:
+			PlayAgainButton.visible = true
