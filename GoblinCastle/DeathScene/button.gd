75ceb697e7850239dc90e5ac0e36ceafcf611191
mike91827 <mike91827.2015@gmail.com>
2026-02-22 16:12:36 -0600

    fixed cabin

--- GoblinCastle/DeathScene/button.gd
+++ GoblinCastle/DeathScene/button.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/MichaelTestScene.tscn")
+	get_tree().change_scene_to_file("res://scenes/actual_main.tscn")
